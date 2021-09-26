6d0a07e443725d849371f2610db0b1f0f4dbbe7c
mehmetturkezer <mehmet.turkezer@gmail.com>
2021-09-26 21:41:51 +0300

    TODO eklendi

---
+++ preload_opt.py
@@ -0,0 +1,14 @@
+# TODO --- Terzaghi's 1D Consolidation Settlement Formulation
+
+# inputs --> H, Cc, Sig_0, Sig_f
+# output --> s (mm)
+
+
+
+# TODO --- Time Rate of Consolidation
+
+
+
+
+
+# TODO --- Radial Consolidation and Preloading
