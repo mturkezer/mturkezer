5625e4e7fc43fc1dfb3b8e47d2d1768ffc2bd888
mehmetturkezer <mehmet.turkezer@gmail.com>
2021-09-27 22:19:27 +0300

    vertical effective stress at mid depth'e kadar yapıldı.

--- preload_opt.py
+++ preload_opt.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # inputs --> H, Cc, Sig_0, Sig_f
 # output --> s (mm)
-
+# TODO - Definition of the Classes
 class NativeSoil():
     def __init__(self, soil_input):
         self.depth = soil_input['depth']
@@ -15,20 +15,22 @@ class NativeSoil():
         self.gamma_sat = soil_input['gamma_sat']
         self.gamma_dry = soil_input['gamma_dry']
 
-    def vertical_stress(self, gwt_elv):
+    # Returns the vertical effective stress increase (at the bottom) due to the buoyant weight of the soil layer.
+    def buoyant_weight(self, gwt_elv):
+        gamma_water = 9.81                              # kN/m^3
         top_elv = self.elevation[0]
         bottom_elv = self.elevation[1]
 
         if gwt_elv >= top_elv:  # Fully saturated layer.
-            ver_str = self.gamma_sat * self.depth
+            buoy_w = (self.gamma_sat - gamma_water) * self.depth
 
         elif gwt_elv <= bottom_elv:
-            ver_str = self.gamma_dry * self.depth  # Completely dry layer.
+            buoy_w = self.gamma_dry * self.depth  # Completely dry layer.
 
         elif gwt_elv in np.linspace(top_elv, bottom_elv, 201):  # GWT in the middle. 5 cm interval
-            ver_str = self.gamma_dry * (top_elv - gwt_elv) + self.gamma_sat * (gwt_elv - bottom_elv)
+            buoy_w = self.gamma_dry * (top_elv - gwt_elv) + (self.gamma_sat-gamma_water) * (gwt_elv - bottom_elv)
 
-        return ver_str
+        return buoy_w
 
 
 class CompressibleSoil(NativeSoil):
@@ -73,19 +75,44 @@ class WickDrain():
         # self.smear_zone
         # self.pattern
 
+def final_settlement(obj):
+    h = obj.depth
+    e0 = obj.e0
+    cc = obj.Cc
+    p0 = obj.ver_eff_stress(gwt_elv=-1)
+
+
+# Sorts the given soil layering according to increasing depth. Returns in a sequenced order of soil layering.
+def borehole(layers):
+    return sorted(layers, key=lambda x: x.elevation[0], reverse= True)
+
+
 
 def main():
-    clay_1_prop = {'gamma_sat': 19,
-                   'gamma_dry':18,
-                   'depth': 40,
-                   'Cc': 0.30,
-                   'Cr': 1.15,
-                   'e0': 1.15,
-                   'OCR': 1,
-                   'K': 0.0001,
-                   'Kr': 0.0005,
-                   'top_elevation': 0
-                   }
+    clay1 = {'gamma_sat': 19,
+           'gamma_dry':18,
+           'depth': 40,
+           'Cc': 0.30,
+           'Cr': 1.15,
+           'e0': 1.15,
+           'OCR': 1,
+           'K': 0.0001,
+           'Kr': 0.0005,
+           'top_elevation': -7
+             }
+
+    sand1 = {'gamma_sat': 19,
+           'gamma_dry':18,
+           'depth': 5,
+           'top_elevation': 0
+             }
+
+    sand2 = {'gamma_sat': 19,
+           'gamma_dry':18,
+           'depth': 2,
+           'top_elevation': -5
+             }
+
 
     preload_prop = {'gamma_sat': 19,
                     'height': 5,
@@ -93,25 +120,30 @@ def main():
                     'base_width': 200
                     }
 
-    cl1 = CompressibleSoil(clay_1_prop)
+    cl1 = CompressibleSoil(clay1)
     preload = Embankment(preload_prop)
+    sm1 = NonCompressibleSoil(sand1)
+    sm2 = NonCompressibleSoil(sand2)
 
-    print(f"CLAY-1 depth: {cl1.depth}, Preload unit weight: {preload.gamma_sat}")
+    borehole1 = [cl1,
+                 sm2,
+                 sm1
+                 ]
+
+    borehole1 = borehole(borehole1)
+
+
+
+
+    print(f"CLAY-1 depth: {cl1.depth}, Preload unit weight: {preload.gamma_sat}\n"
+          f"SM-1 vertical effective stress increase: {sm1.buoyant_weight(gwt_elv=-1)}\n"
+          f"SM-2 vertical effective stress increase: {sm2.buoyant_weight(gwt_elv=-1)}\n"
+          f"Borehole from top to bottom: {borehole1}")
 
 
 print(f"__name__ value: {__name__}")
 if __name__ == '__main__':
     main()
-
-#
-# H = 5
-# gamma_soil = 19
-# gamma_fill = 20
-# height_fill = 10
-# Cc = 0.30
-# p_0 = gamma * (H/2)
-# pload = gamma_fill * height_fill
-
 
 # TODO --- Time Rate of Consolidation
 
